6f970a2b589caa2f67f1be82a6362d19daaa3a07
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-08-03 21:43:08 +0900

    rename files

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-ruby generate_freq_tables.rb > freq_table.rb
-ruby generate_wave_tables.rb > wave_table.rb
+ruby generate_freq_table.rb > freq_table.rb
+ruby generate_wave_table.rb > wave_table.rb
 ruby midi_controller_scale.rb
 ruby main.rb < midi_stream.bin
