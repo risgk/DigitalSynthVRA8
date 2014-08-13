b8cae5192955f0bdff10c99ee33ef5a448677901
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-08-13 12:51:52 +0900

    delete one EG

--- ruby/main.rb
+++ ruby/main.rb
@@ -19,8 +19,7 @@ File::open("a.wav","w+b") do |file|
   mixer = Mixer.new
   filter = Filter.new
   amp = Amp.new
-  filter_eg = EG.new
-  amp_eg = EG.new
+  eg = EG.new
 
   osc1.set_waveform(Osc::TRIANGLE)
   osc2.set_waveform(Osc::SAW)
@@ -32,8 +31,7 @@ File::open("a.wav","w+b") do |file|
   filter.set_cutoff_freq(64)
   filter.set_resonance(true)
   filter.set_envelope_switch(true)
-  filter_eg.set_adsr(32, 127, 0, 112)
-  amp_eg.set_adsr(32, 127, 0, 112)
+  eg.set_adsr(32, 127, 0, 112)
 
   midi_in_prev = 0xFF
   midi_in_pprev = 0xFF
@@ -45,25 +43,22 @@ File::open("a.wav","w+b") do |file|
       osc1.note_on(note_number)
       osc2.note_on(note_number)
       osc3.note_on(note_number)
-      filter_eg.note_on
-      amp_eg.note_on
+      eg.note_on
     end
     if (midi_in_pprev == MIDI_NOTE_OFF && midi_in_prev <= 0x7F && b <= 0x7F)
       osc1.note_off
       osc2.note_off
       osc3.note_off
-      filter_eg.note_off
-      amp_eg.note_off
+      eg.note_off
     end
     midi_in_pprev = midi_in_prev
     midi_in_prev = b
 
     for i in (0...10) do
       level = mixer.clock(osc1.clock, osc2.clock, osc3.clock)
-      feg_output = filter_eg.clock
-      level = filter.clock(level, feg_output)
-      aeg_output = amp_eg.clock
-      level = amp.clock(level, aeg_output)
+      eg_output = eg.clock
+      level = filter.clock(level, eg_output)
+      level = amp.clock(level, eg_output)
 
       file.write([level * 128].pack("S"))
     end
