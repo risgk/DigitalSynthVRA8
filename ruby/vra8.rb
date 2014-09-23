cdb77c0f86eafaa093c5a18a59ec6318067ed28c
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-09-23 20:17:29 +0900

    Add a workaround for midi-jruby (0.0.12)

--- ruby/vra8.rb
+++ ruby/vra8.rb
@@ -22,6 +22,24 @@ if ARGV.length == 1
   end
 else
   require 'unimidi'
+  # workaround: midi-jruby (0.0.12) cannot receive a data byte 2 with a value of 0
+  module MIDIJRuby
+    class Input
+      class InputReceiver
+        def send(msg, timestamp = -1)
+          if msg.respond_to?(:get_packed_msg)
+            m = msg.get_packed_msg
+            @buf << [m & 0xFF, (m & 0xFF00) >> 8, (m & 0xFF0000) >> 16].take(msg.get_length)
+          else
+            str = String.from_java_bytes(msg.get_data)
+            arr = str.unpack("C" * str.length)
+            arr.insert(0, msg.get_status)
+            @buf << arr
+          end
+        end
+      end
+    end
+  end
   require 'thread'
   q = Queue.new
   t = Thread.new do
