c07357a57a474c118634b9d9c78a6776675e0a1e
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-02-15 15:18:08 +0900

    Renew FFT implementation

--- fft/fft.rb
+++ fft/fft.rb
@@ -1,8 +1,21 @@
 def fft(a)
-  n = a.size
-  return a if n == 1
-  w = Complex.polar(1, -2 * Math::PI / n)
-  a1 = fft((0 .. n / 2 - 1).map {|i| a[i] + a[i + n / 2] })
-  a2 = fft((0 .. n / 2 - 1).map {|i| (a[i] - a[i + n / 2]) * (w ** i) })
-  a1.zip(a2).flatten
+  ditfft2(a, a.size, 1)
+end
+
+# refs http://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm
+# Cooley–Tukey FFT algorithm - Wikipedia, the free encyclopedia
+def ditfft2(x, n, s)
+  result = []
+  if n == 1 then
+    result[0] = x[0]
+  else
+    result += ditfft2(x,         n / 2, 2 * s)
+    result += ditfft2(x.drop(s), n / 2, 2 * s)
+    for k in 0..(n / 2 - 1) do
+      t = result[k]
+      result[k]         = t + (Math::E ** Complex(0, -2 * Math::PI * k / n)) * result[k + n / 2]
+      result[k + n / 2] = t - (Math::E ** Complex(0, -2 * Math::PI * k / n)) * result[k + n / 2]
+    end
+  end
+  result
 end
